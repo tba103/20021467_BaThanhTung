49f5b820817cbc7e12107e54d210652716ff308a
BaTung <20021467@vnu.edu.vn>
2021-03-20 22:48:46 +0700

    Add bt tuan 7

--- Bai tap tuan 7/Cau4.cpp
+++ Bai tap tuan 7/Cau4.cpp
@@ -11,7 +11,8 @@ struct Student
   }
   void GetInf()
   {
-      string s,y;
+      string s;
+      string y = "";
       getline(cin,s);
     int i=0;
     for( i; i < s.size(); i++)
@@ -19,9 +20,7 @@ struct Student
         else 
         {
             MSSV = y;
-            
             i+=2;
-            cout << y <<endl;
             y="";
             break;
             
@@ -31,31 +30,42 @@ struct Student
         else 
         {
             Hoten = y;
-            
-            i+= 2;
-             cout << y << endl;
-             y="";
+            i+= 2;        
+            y="";
             break;
         }
     for(    i; i < s.size(); i++)
-        if( s[i] != ',') y = y+ s[i];
-        else 
-        {
-            GPA = y;       
-            i+=2;
-             cout << y << endl;
-             y="";
-            break;
-        }
-  }
+        y = y+ s[i];
+    GPA = y;
+    cout  << MSSV << endl<< Hoten << endl;
+    
+}
 };
+Student bigger( Student a, Student b)
+{
+	if( (a.GPA) > (b.GPA)) return a;
+	return b;	
+}
+Student best( Student *s, int n)
+{
+	Student *p = s;
+	Student res =  s[0];
+	while ( p <= &s[n-1])
+	{
+		res = bigger((*p), *(p+1));
+		p++;
+	}
+	return res;
+}
 int main()
 {
     int n;
     Student s[100];
     cin >> n;
-    for( int i=0 ; i < n; i++)
-        s[i].GetInf();
+    for( int i=0 ; i <= n; i++)
+      s[i].GetInf();
+    Student x = best(s,n);
+    cout << x.Hoten << endl;
     return 0;
 }
 

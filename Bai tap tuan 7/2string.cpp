d4c94ae52d7e3a5041a67cf1b04527c38d1ee795
BaTung <20021467@vnu.edu.vn>
2021-03-20 23:03:15 +0700

    Add 2string

---
+++ Bai tap tuan 7/2string.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int main ()
+{
+  std::string foo = "4.324324";
+  std::string bar = "2.43434";
+
+  if (foo==bar) std::cout << "foo and bar are equal\n";
+  if (foo!=bar) std::cout << "foo and bar are not equal\n";
+  if (foo< bar) std::cout << "foo is less than bar\n";
+  if (foo> bar) std::cout << "foo is greater than bar\n";
+  if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
+  if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
+
+  return 0;
+}

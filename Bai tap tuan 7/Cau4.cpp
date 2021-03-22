c866c8f7a555db32053c2b3c808abfaf002aaec4
BaTung <20021467@vnu.edu.vn>
2021-03-22 23:32:57 +0700

    Add Final

--- Bai tap tuan 7/Cau4.cpp
+++ Bai tap tuan 7/Cau4.cpp
@@ -1,71 +1,143 @@
 #include <iostream>
-#include <string>
 using namespace std;
-struct Student
-{
-  string MSSV;
-  string Hoten;
-  string GPA;
-  Student()
-  {
-  }
-  void GetInf()
-  {
-      string s;
-      string y = "";
-      getline(cin,s);
-    int i=0;
-    for( i; i < s.size(); i++)
-        if( s[i] != ',') y = y+ s[i];
-        else 
-        {
-            MSSV = y;
-            i+=2;
-            y="";
-            break;
-            
-        }
-    for( i; i < s.size(); i++)
-        if( s[i] != ',') y = y+ s[i];
-        else 
-        {
-            Hoten = y;
-            i+= 2;        
-            y="";
-            break;
-        }
-    for(    i; i < s.size(); i++)
-        y = y+ s[i];
-    GPA = y;
-    cout  << MSSV << endl<< Hoten << endl;
-    
-}
-};
-Student bigger( Student a, Student b)
-{
-	if( (a.GPA) > (b.GPA)) return a;
-	return b;	
-}
-Student best( Student *s, int n)
-{
-	Student *p = s;
-	Student res =  s[0];
-	while ( p <= &s[n-1])
+struct BigInteger{
+	int sign;
+	string number;	
+	BigInteger()
 	{
-		res = bigger((*p), *(p+1));
-		p++;
+	}
+	void Nhap()
+	{
+		cin >> sign >> number;
+	}
+};
+string Summation( string a, string b)
+{
+	int k=0;
+	string s="";
+	while ( a.size() != b.size())
+	{
+		if( a.size() < b.size())  a = '0' + a;
+		else  b = '0' + b;
+	}
+	char * tro1 =  &a[a.size()-1];
+	char * tro2 = &b[b.size()-1];
+	while ( tro1 >=  &a[0])
+	{
+	//	cout << *tro1 << "---" << *tro2 << endl;
+		if ( (*tro1 -'0' + *tro2-'0' +k)  >= 10) 
+		{
+			char x = (*tro1-'0' + *tro2-'0' -10 +k) +'0';
+			s =  x+s;
+			k=1;
+		}
+		else 
+		{
+			char x = (*tro1-'0' +*tro2-'0' +k) +'0';
+			s = x+s;
+			k=0;
+		}
+		tro1--;
+		tro2--;
+	}
+	if ( 1 == k) s = '1' + s;
+	return s;
+}
+string Subtraction( string a, string b)
+{
+	int k=0;
+	string s="", res = "";
+	char * tro1 =  &a[a.size()-1];
+	char * tro2 = &b[b.size()-1];
+	while ( tro2 >=  &b[0])
+	{
+		//cout << *tro1 << "---" << *tro2 << endl;
+		if ( k+ ((*tro1 -'0') - (*tro2-'0')) < 0) 
+		{
+			char x = ((*tro1-'0') - (*tro2-'0') +10 +k) +'0';
+			s =  x+s;
+			k=-1;
+		}
+		else 
+		{
+			char x = ((*tro1-'0') - (*tro2-'0') +k) +'0';
+			s = x+s;
+			k=0;
+		}
+		tro1--;
+		tro2--;
+	}
+	while ( tro1 >= &a[0])
+	{
+		*tro1 += k;
+		s = *tro1 + s;
+		k=0;
+		tro1--;
+	}
+	tro1 = &s[0];
+	while ( tro1 <= &s[s.size()-1])
+	{
+		if ( *tro1 != '0') break;
+		tro1++;
+	}
+	while ( tro1 <= &s[s.size()-1])
+	{
+		res += *tro1;
+		tro1++;
+	}
+		if ( res.empty()) res = "0";
+	//cout << "---" << res << "---" << endl;
+
+	return res;
+}
+BigInteger sum( BigInteger first, BigInteger second)
+{
+	BigInteger res;
+	if( first.sign == second.sign) 
+	{
+			res.sign = first.sign;
+			res.number = Summation( first.number, second.number);
+			//cout <<"---" << res.sign <<"---"<< res.number << endl;
+	}
+	else if ( first.sign == 1) 
+	{
+		if( (first.number).size() > (second.number).size() || ((first.number).size() == (second.number).size() && first.number >= second.number ) )
+		{
+			res.sign = 1;
+			res.number = Subtraction(first.number, second.number);
+		 } 
+		else if((first.number).size() < (second.number).size() || ((first.number).size() == (second.number).size() && first.number <= second.number ) )
+		{
+			res.sign = 0;
+			res.number = Subtraction(second.number, first.number);
+		}
+		
+	}
+	else 
+	{
+		if( (first.number).size() > (second.number).size() || ((first.number).size() == (second.number).size() && first.number >= second.number ) )
+		{
+			res.sign = 0;
+			res.number = Subtraction(first.number, second.number);
+		 } 
+		else if((first.number).size() < (second.number).size() || ((first.number).size() == (second.number).size() && first.number <= second.number ) )
+		{
+			res.sign = 1;
+			res.number =  Subtraction(second.number, first.number);
+		}
 	}
 	return res;
 }
-int main()
-{
-    int n;
-    Student s[100];
-    cin >> n;
-    for( int i=0 ; i <= n; i++)
-      s[i].GetInf();
-    Student x = best(s,n);
-    cout << x.Hoten << endl;
-    return 0;
+int main(){
+	BigInteger first,second;
+	first.Nhap();
+	second.Nhap();
+	BigInteger res = sum(first,second);
+	if ( res.number == "0") cout << '0';
+	else 
+	{
+	if ((res.sign ==0) ) cout << '-' ;
+	cout << res.number << endl;
+	}
+	return 0;
 }
-
--- Bai tap tuan 7/Cau5.cpp
+++ Bai tap tuan 7/Cau5.cpp
@@ -1,137 +1,73 @@
 #include <iostream>
+#include <string>
 using namespace std;
-struct BigInteger{
-	int sign;
-	string number;	
-	BigInteger()
-	{
-	}
-	void Nhap()
-	{
-		cin >> sign >> number;
-	}
+struct Student
+{
+  string MSSV;
+  string Hoten;
+  string GPA;
+  Student()
+  {
+  }
+  void GetInf()
+  {
+      string s;
+      string y = "";
+      getline(cin,s);
+    int i=0;
+    for( i; i < s.size(); i++)
+        if( s[i] != ',') y = y+ s[i];
+        else 
+        {
+            MSSV = y;
+            i+=2;
+            y="";
+            break;
+            
+        }
+    for( i; i < s.size(); i++)
+        if( s[i] != ',') y = y+ s[i];
+        else 
+        {
+            Hoten = y;
+            i+= 2;        
+            y="";
+            break;
+        }
+    for(    i; i < s.size(); i++)
+        y = y+ s[i];
+    GPA = y;
+
+    
+}
 };
-string Summation( string a, string b)
+Student bigger( Student a, Student b)
 {
-	int k=0;
-	string s="";
-	while ( a.size() != b.size())
-	{
-		if( a.size() < b.size())  a = '0' + a;
-		else  b = '0' + b;
-	}
-	char * _a =  &a[a.size()-1];
-	char * _b = &b[b.size()-1];
-	while ( _a >=  &a[0])
-	{
-		if ( (*_a -'0' + *_b-'0' +k)  >= 10) 
-		{
-			char x = (*_a-'0' + *_b-'0' -10 +k) +'0';
-			s =  x+s;
-			k=1;
-		}
-		else 
-		{
-			char x = (*_a-'0' +*_b-'0' +k) +'0';
-			s = x+s;
-			k=0;
-		}
-		_a--;
-		_b--;
-	}
-	if ( 1 == k) s = '1' + s;
-	return s;
+//	cout << a.GPA << "___" << b.GPA << "___";
+	if( (a.GPA) > (b.GPA)) return a;
+	return b;	
 }
-string Subtraction( string a, string b)
+Student best( Student *s, int n)
 {
-	int k=0;
-	string s="", res = "";
-	while ( a.size() != b.size())
+	Student *p = s;
+	Student res =  s[0];
+	while ( p <= &s[n])
 	{
-		if( a.size() < b.size())  a = '0' + a;
-		else  b = '0' + b;
-	}
-	char * _a =  &a[a.size()-1];
-	char * _b = &b[b.size()-1];
-	while ( _b >=  &b[0])
-	{
-		if ( k+ ((*_a -'0') - (*_b-'0')) < 0) 
-		{
-			char x = ((*_a-'0') - (*_b-'0') +10 +k) +'0';
-			s =  x+s;
-			k=-1;
-		}
-		else 
-		{
-			char x = ((*_a-'0') - (*_b-'0') +k) +'0';
-			s = x+s;
-			k=0;
-		}
-		_a--;
-		_b--;
-	}
-	char * _res = &s[0];
-	while ( _res <= &s[s.size()-1])
-	{
-		if ( *_res != '0') break;
-		_res++;
-	}
-	while ( _res <= &s[s.size()-1])
-	{
-		res += *_res;
-		_res++;
-	}
-		if ( res.empty()) res = "0";
-	return res;
-}
-BigInteger sum( BigInteger first, BigInteger second)
-{
-	BigInteger res;
-	if( first.sign == second.sign) 
-	{
-			res.sign = first.sign;
-			res.number = Summation( first.number, second.number);
-			//cout <<"---" << res.sign <<"---"<< res.number << endl;
-	}
-	else if ( first.sign == 1) 
-	{
-		if( (first.number).size() > (second.number).size() || ((first.number).size() == (second.number).size() && first.number >= second.number ) )
-		{
-			res.sign = 1;
-			res.number = Subtraction(first.number, second.number);
-		 } 
-		else if((first.number).size() < (second.number).size() || ((first.number).size() == (second.number).size() && first.number <= second.number ) )
-		{
-			res.sign = 0;
-			res.number = Subtraction(second.number, first.number);
-		}
-		
-	}
-	else 
-	{
-		if( (first.number).size() > (second.number).size() || ((first.number).size() == (second.number).size() && first.number >= second.number ) )
-		{
-			res.sign = 0;
-			res.number = Subtraction(first.number, second.number);
-		 } 
-		else if((first.number).size() < (second.number).size() || ((first.number).size() == (second.number).size() && first.number <= second.number ) )
-		{
-			res.sign = 1;
-			res.number =  Subtraction(second.number, first.number);
-		}
+		res = bigger((*p), res);
+	//	cout << res.GPA << endl;
+		p++;
 	}
 	return res;
 }
-int main(){
-	BigInteger first,second;
-	first.Nhap();
-	second.Nhap();
-	BigInteger res = sum(first,second);
-	if ( res.number == "0") cout << '0';
-	else 
-	{
-	if ((res.sign ==0) ) cout << '-' ;
-	cout << res.number << endl;
-	}
-	return 0;
+int main()
+{
+    int n;
+    Student s[100];
+    cin >> n;
+    for( int i=0 ; i <= n; i++)
+      s[i].GetInf();
+    Student x = best(s,n);
+    cout << x.Hoten << endl;
+    return 0;
 }
+
